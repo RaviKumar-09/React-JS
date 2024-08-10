927045fa65693039e68924d4f2bac2115582a747
RaviKumar <ravikumarainapudi97@gmail.com>
2024-08-10 22:50:12 +0530

    my app

---
+++ My-App/myapp/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+//import logo from './logo.svg';
+//import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello I am Ravi Kumar</h1>
+      <h1>I am new learner of React Js </h1>
+    </div>
+  );
+}
+
+export default App;
