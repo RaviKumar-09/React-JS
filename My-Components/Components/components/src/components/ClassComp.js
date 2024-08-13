6e57823d251a8482d8552e9c8fe5e0408eae0274
RaviKumar <ravikumarainapudi97@gmail.com>
2024-08-13 22:48:17 +0530

    changing Paths

---
+++ My-Components/Components/components/src/components/ClassComp.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export class ClassComp extends React.Component{
+    render(){
+        return <p>This is a Class component</p>
+    }
+}
+
+export class ClassComp1 extends React.Component{
+    render(){
+        return <p>Hey I'm another Class component</p>
+    }
+}
+
