6c1e9865c0006b19a56877ba8475e82ac5b09ca1
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-09 20:37:52 +0530

    creating Non-Veg Items

--- my-react-app/src/Food.jsx
+++ my-react-app/src/Food.jsx
@@ -8,6 +8,12 @@ function Food(){
                 <li>Brinzle</li>
                 <li>Ladyfinger</li>
                 <li>Onions</li>
+
+                <h1>Non-Veg Items</h1>
+                <li>Chicken</li>
+                <li>Mottoun</li>
+                <li>Fish</li>
+                <li>Egg</li>
             </ul>
         </div>
     );
