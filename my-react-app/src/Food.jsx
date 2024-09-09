93e160b5f1e659f50e2174bca69cdb72de8e6926
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-09 20:34:15 +0530

    creating list of Vegetable Items

---
+++ my-react-app/src/Food.jsx
@@ -0,0 +1,16 @@
+
+function Food(){
+    return(
+        <div>
+            <ul>
+                <h1>Vegetable Items</h1>
+                <li>Brocali</li>
+                <li>Brinzle</li>
+                <li>Ladyfinger</li>
+                <li>Onions</li>
+            </ul>
+        </div>
+    );
+
+}
+export default Food
