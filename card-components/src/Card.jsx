51ba5ea6021bf8e01aa2868cb802ef722cc9d3ac
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-09 21:31:29 +0530

    creating Card fucntion to Crad compomet

---
+++ card-components/src/Card.jsx
@@ -0,0 +1,12 @@
+function Card(){
+    return(
+        <div className="card">
+            <img src="" alt="" />
+            <h2>Ravi Kumar</h2>
+            <p>I am a Developer and play vedio games.</p>
+        </div>
+
+    );
+
+}
+export default Card
