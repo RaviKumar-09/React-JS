f9342e8f2aa6a56e314d371d2fc5bf0a80727b8a
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-09 21:35:44 +0530

    importing image in assests folder

--- card-components/src/Card.jsx
+++ card-components/src/Card.jsx
@@ -1,7 +1,7 @@
 function Card(){
     return(
         <div className="card">
-            <img src="" alt="" />
+            <img src="" alt="profile picture" />
             <h2>Ravi Kumar</h2>
             <p>I am a Developer and play vedio games.</p>
         </div>
