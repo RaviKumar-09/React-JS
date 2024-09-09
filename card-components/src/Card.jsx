766a0cb6cc50bfa3d11ff50a355827ea4cf7f67a
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-09 21:40:42 +0530

    creating a CARD

--- card-components/src/Card.jsx
+++ card-components/src/Card.jsx
@@ -1,8 +1,8 @@
-
+import profilePic from './assets/profile.png'
 function Card(){
     return(
         <div className="card">
-            <img src="" alt="profile picture" />
+            <img src={profilePic} alt="profile picture" />
             <h2>Ravi Kumar</h2>
             <p>I am a Developer and play vedio games.</p>
         </div>
