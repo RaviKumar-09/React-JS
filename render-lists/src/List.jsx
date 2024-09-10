43e0d362108d38534807a8a6881eec8869b5a7cc
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-10 12:26:54 +0530

    {fruit.name}: &nbsp;                                             <b>{fruit.calories}</b></li>);

--- render-lists/src/List.jsx
+++ render-lists/src/List.jsx
@@ -1,12 +1,14 @@
 function List(){
     const fruits = [{id:1,name:"apple" , calories: 95}, 
-                    {nid:2,ame:"Banana", calories: 45}, 
+                    {id:2,name:"Banana", calories: 45}, 
                     {id:3,name:"orange", calories:105}, 
                     {id:4,name:"pineapple", calories:134}];
 
     //fruits.sort()
 
-    const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}</li>);
+    const listItems = fruits.map(fruit => <li key={fruit.id}>
+                                            {fruit.name}: &nbsp;
+                                            <b>{fruit.calories}</b></li>);
     
     return(<ol>{listItems}</ol>);
 }
