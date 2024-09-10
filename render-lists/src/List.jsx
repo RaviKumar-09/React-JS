0bb20c9767f276e4580149092d1e69cd43df7cff
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-10 12:14:40 +0530

    sorting method

--- render-lists/src/List.jsx
+++ render-lists/src/List.jsx
@@ -1,6 +1,8 @@
 function List(){
     const fruits = ["apple", "Banana", "orange", "pineapple"];
 
+    //fruits.sort()
+
     const listItems = fruits.map(fruit => <li>{fruit}</li>);
     
     return(<ol>{listItems}</ol>);
