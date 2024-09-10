c24055a3b052e44fa0de22c60e1bb107a55f36ba
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-10 12:06:40 +0530

    unorder list of fruits

--- render-lists/src/List.jsx
+++ render-lists/src/List.jsx
@@ -1,7 +1,9 @@
 function List(){
-    const fruits = ["apple", "Banana", "orange", "pineapple"]
+    const fruits = ["apple", "Banana", "orange", "pineapple"];
 
-    return(fruits);
+    const listItems = fruits.map(fruit => <li>{fruit}</li>);
+    
+    return(<ul>{listItems}</ul>);
 }
 
 export default List
