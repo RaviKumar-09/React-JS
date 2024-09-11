dab32083c927b127982d2b7c2f88efcc915c5ccb
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-11 19:04:50 +0530

    const [isEmployed, setIsEmployed] = useState(false);

--- use-state/src/MyComponent.jsx
+++ use-state/src/MyComponent.jsx
@@ -3,8 +3,8 @@ import React, {useState} from "react"
 function MyComponent(){
 
     const [name, setName] = useState("Guest");
-
     const [age, setAge] = useState(0);
+    const [isEmployed, setIsEmployed] = useState(false);
 
     const updateName = () => {
         setName("Ravi");
@@ -13,8 +13,10 @@ function MyComponent(){
     function IncrementAge() {
         setAge(age + 1);
 
+    }
 
-        
+    const toggleEmployed = () =>{
+        setIsEmployed(!isEmployed);
     }
 
         return(
@@ -24,6 +26,10 @@ function MyComponent(){
 
                 <p>Name: {age}</p>
                 <button onClick={IncrementAge}>Increment Age</button>
+
+                <p>Is employed: {isEmployed ? "Yes" : "No"}</p>
+                <button onClick={toggleEmployed}>Increment Age</button>
+                
             </div>
         );
 
