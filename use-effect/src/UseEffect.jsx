6149c4e57bc15c6ee2a79f24083359b2e7e8341d
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-11 19:50:15 +0530

    create const addCount = () =>

--- use-effect/src/UseEffect.jsx
+++ use-effect/src/UseEffect.jsx
@@ -1,6 +1,17 @@
 import React, {useState, useEffect} from "react";
 
 function UseEffect(){
+    const [count, setCount] = useState();
+    const addCount = () =>{
+        setCount(c => c + 1);
+
+    }
+    return(
+        <>
+        <p>Cont: {count}</p>
+        <button onClick={addCount}>Add</button>
+        </>
+    );
 
 }
 
