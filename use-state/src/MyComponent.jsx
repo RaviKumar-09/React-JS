d895afbbd102a63b0bfed3dde953653f35ffd815
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-11 18:56:43 +0530

    IncrementAge function created

--- use-state/src/MyComponent.jsx
+++ use-state/src/MyComponent.jsx
@@ -4,14 +4,26 @@ function MyComponent(){
 
     const [name, setName] = useState("Guest");
 
+    const [age, setAge] = useState(0);
+
     const updateName = () => {
         setName("Ravi");
+    }
+
+    function IncrementAge() {
+        setAge(age + 1);
+
+
+        
     }
 
         return(
             <div>
                 <p>Name: {name}</p>
                 <button onClick={updateName}>Set Name</button>
+
+                <p>Name: {age}</p>
+                <button onClick={IncrementAge}>Increment Age</button>
             </div>
         );
 
