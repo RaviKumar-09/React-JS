c7d908305a86acf3ead8fc0a4328b1b64573368e
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-12 12:15:40 +0530

    return <h2>{`Bye ${props.user}`}</h2> comD

--- use-context/src/ComponentD.jsx
+++ use-context/src/ComponentD.jsx
@@ -1,8 +1,9 @@
-function ComponentD(){
+function ComponentD(props){
 
     return(
         <div className="box">
             <h1>ComponentD</h1>
+            <h2>{`Bye ${props.user}`}</h2>
         </div>
     );
 
