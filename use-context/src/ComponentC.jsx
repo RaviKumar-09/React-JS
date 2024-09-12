5508eb057a869e9e9d46cd4de36b2e5a34e11f27
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-12 12:28:52 +0530

    React useContext() hook

--- use-context/src/ComponentC.jsx
+++ use-context/src/ComponentC.jsx
@@ -1,10 +1,14 @@
 import ComponentD from "./ComponentD";
+import { useContext } from "react";
+import { UserContext } from "./ComponentA";
 
 function ComponentC(){
+    const user = useContext(UserContext);
 
     return(
         <div className="box">
             <h1>ComponentC</h1>
+            <h2>{`Hello again and again ${user}`}</h2>
             <ComponentD />
         </div>
     );
