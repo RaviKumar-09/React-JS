9dbe33b485d29d62060c38476566341c3889c277
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-12 13:00:23 +0530

    const ref = useRef(0);

--- use-ref/src/MyComponent.jsx
+++ use-ref/src/MyComponent.jsx
@@ -1,6 +1,24 @@
-import { useState, useEffect, useRef } from "react"
+import {  useEffect, useRef } from "react"
 
 function MyComponent(){
+
+    const ref = useRef(0);
+
+    useEffect( function() {
+        console.log("Conponent Render")
+
+    })
+
+    function handleClick() {
+        ref.current++;
+
+    }
+
+    return (
+    
+        <button onClick={handleClick}>Click me!</button>
+
+    );
 
 }
 export default MyComponent
