4d076dbc58e10895b55e5511c51556347197c0ab
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-12 13:10:35 +0530

    inputref.current.style.backgroundColor = "red"

--- use-ref/src/MyComponent.jsx
+++ use-ref/src/MyComponent.jsx
@@ -2,29 +2,30 @@ import {  useEffect, useRef } from "react"
 
 function MyComponent(){
 
-    const imputref = useRef(null);
+    const inputref = useRef(null);
     /*console.log(ref)*/
 
     useEffect( function() {
         console.log("Conponent Render")
 
-    })
+    });
 
     function handleClick() {
         /*ref.current++;*/
         /*console.log(ref.current)*/
+        inputref.current.focus();
+        inputref.current.style.backgroundColor = "red"
 
-    }
+    };
 
     return (<>
     
         <button onClick={handleClick}>Click me!</button>
         <br />
         <br />
-        
-        <input ref={imputref}/>
+        <input ref={inputref}/>
 
         </>);
 
-}
+};
 export default MyComponent
