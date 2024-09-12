7791a30b416f5a9026a14f5209a8bd1e349af349
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-12 12:01:51 +0530

    function ComponentD() created

--- use-context/src/ComponentD.jsx
+++ use-context/src/ComponentD.jsx
@@ -1,4 +1,4 @@
-function ComponentA(){
+function ComponentD(){
 
     return(
         <div className="box">
@@ -7,4 +7,4 @@ function ComponentA(){
     );
 
 }
-export default ComponentA
+export default ComponentD
